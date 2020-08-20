e1c10c20205cbd7d12dd28607b58e92c45b92e15
Zeeshan Noor <zeeshannoor7831@gmail.com>
2020-08-19 23:25:50 -0700

    Shoe Store

---
+++ src/Components/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+   <br/> 
+   <h2>About Us..!!!</h2>
+   <hr/>
+   <p>
+     Here at ‘Organization Name’.com, we realize that style and prosperity begin with the correct shoes. We likewise understand that effectively finding the size and style to meet your requirements is vital to your web based shopping knowledge. Since beginning our web based business website in 2009, we’ve been endeavoring to present to you that perfect shopping experience.
+     <br/>
+We pride ourselves on conveying hard-to-discover styles, sizes and widths since we realize that each individual’s needs contrast. Regardless of whether you’re experiencing issues looking over our vast determination of different shoes or you have a straightforward inquiry, our client benefit group is prepared and willing to help.
+<br/>
+Some time before our online business, our family shoe stores, fatherly shoe store Footwear in Indianapolis, IN, opened its entryways in 1886. Today we are glad to state that we have the longest constantly running shoe store in the United States. The life span of our physical business is a demonstration of our duty to you and the majority of our clients. We realize that on the off chance that you are not content with our items and administrations, we are not carrying out our responsibility.</p>
+<br/>  
+<br/><br/>  
+<hr/>
+<div className="Productscontainer">  
+  <h2>Home</h2>
+  <h2>About Us</h2>
+  <h2>Contact Us</h2>
+  </div>
+
+ </div>
+  );
+}
+
+export default About;
